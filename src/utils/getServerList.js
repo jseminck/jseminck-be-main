33c2c1f8d81aa4541a3063fe2aeb456677fe2bcd
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-07-24 17:53:40 +0300

    Add Ancient Tundra

--- src/utils/getServerList.js
+++ src/utils/getServerList.js
@@ -3,6 +3,7 @@ export default function() {
     // {name: 'offline', heroku: 'doesnot-exist-93989'},
     {name: 'expenses', heroku: 'calm-reef-93989'},
     {name: 'authentication', heroku: 'pacific-refuge-84094'},
-    {name: 'aws', heroku: 'powerful-meadow-99607'}
+    {name: 'aws', heroku: 'powerful-meadow-99607'},
+    {name: 'links', heroku: 'ancient-tundra-31260'}
   ];
 }
