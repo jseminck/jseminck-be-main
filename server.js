444974ba42c7eabf03243d89f05109cc90a9213f
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-26 23:38:16 +0300

    Re-add prebuild

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
-
 var app = express();
+
 app.use(express.static('dist'));
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 1234;
 app.listen(port);
-console.log("Server running at http://127.0.0.1:" + 1234); // eslint-disable-line no-console
+console.log("Server running at http://127.0.0.1:" + port); // eslint-disable-line no-console
--- tools/build.js
+++ tools/build.js
@@ -1,6 +1,7 @@
 // More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
 // Allowing console calls below since this is a build file.
 /* eslint-disable no-console */
+
 import webpack from 'webpack';
 import config from '../webpack.config.prod';
 import {chalkError, chalkSuccess, chalkWarning, chalkProcessing} from './chalkConfig';
