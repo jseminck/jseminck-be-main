08f07a90aff70f154303248a8f6a72e32ad473db
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-26 22:06:30 +0300

    Rewrite server list layout

--- src/components/Servers/ServerOverviewItem.js
+++ src/components/Servers/ServerOverviewItem.js
@@ -8,39 +8,34 @@ export default class ServerOverviewItem extends React.Component {
 
     render() {
         return (
-            <div className="server-item">
-                <div>
+            <div className="server-item-container">
+                <div className="server-status" style={{backgroundColor: this.getBackgroundColor()}}></div>
+                <div className="server-icon">
                     <i className={this.getFontAwesomeIcon()} />
                 </div>
-                <div className="server-item-info">
-                    <div>
-                        <a href={`http://${this.props.server.link}`}>
-                            {this.props.server.name}
-                        </a>
-                    </div>
-                    <div>
-                        {this.getIcon(this.props.server.state)}
-                        {this.getDelay()}
-                    </div>
+                <div className="server-name">
+                    <a href={`http://${this.props.server.link}`}>
+                        {this.props.server.name}
+                    </a>
                 </div>
             </div>
         );
     }
 
     getFontAwesomeIcon() {
-        return `fa fa-2x fa-${this.props.server.faIcon}`;
+        return `fa fa-3x fa-${this.props.server.faIcon}`;
     }
 
-    getIcon() {
+    getBackgroundColor() {
         const {state} = this.props.server;
         if (state === "loading") {
-            return <img src={require("./loading.png")} className="server-item-info-icon loading-icon" />;
+            return "orange";
         }
         if (state === "online") {
-            return <img src={require("./online.svg")} className="server-item-info-icon online-icon" />;
+            return "green";
         }
         if (state === "offline") {
-            return <img src={require("./offline.svg")} className="server-item-info-icon offline-icon" />;
+            return "red";
         }
     }
 
